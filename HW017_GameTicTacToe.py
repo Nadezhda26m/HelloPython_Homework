5be90bbff4f1370e9c9a807b8fd9d5844c55518c
Nadezhda26m <nadezhda-26m@mail.ru>
2022-12-19 12:55:44 +0300

    HW 017 add functions

--- HW017_GameTicTacToe.py
+++ HW017_GameTicTacToe.py
@@ -1,7 +1,83 @@
 # Сделать игру крестики-нолики
+#
+# Алгоритм
+# 1. Создаем пустое игровое поля заданного размера, список списков,
+# заполняем значением по умолчанию (-)
+# 2. Определяем игроков, их роли (символ) и кто ходит первым
+# 3. Игра продолжается до тех пор, пока не объявится победитель или
+# не исчерпаются все ходы, максимальное количество ходов - количество ячеек
+# 4. Игрок и компьютер по очереди выбирают ячейку, она должна быть внутри поля
+# и не занятой
+# 5. После каждого хода проверяются все условия победы, так как их может быть
+# больше одного. Ищем строку, столбик или диагональ, заполненную целиком 'O' или 'X'.
+# Когда хотя бы одно условие победы выполняется, выводим выигрышную комбинацию
+# (или несколько) и победителя. Завершаем игру выходом из цикла
+# 6. Если в конце игры победитель не определился, то объявляется ничья
 
 from random import randint
 import time
+
+# Печать квадратного игрового поля с нумерацией
+def print_field_square(field):
+    size_field = len(field)
+    for i in range(-1, size_field):
+        if i == -1:
+            print(' ', end=' ')
+            for p in range(1, size_field + 1):
+                print(p, end=' ')
+            print()
+        else:
+            print(f'{i + 1}', end=' ')
+            for k in range(0, size_field):
+                print(field[i][k], end=' ')
+            print()
+
+def find_win_row(field, value_default):
+    size_field = len(field)
+    for i in range(size_field):
+        if field[i][0] != value_default:
+            for k in range(1, size_field):
+                if field[i][k] != field[i][0]:
+                    break
+            else:
+                return (f'строка {i + 1}', field[i][0])
+
+def find_win_col(field, value_default):
+    size_field = len(field)
+    for i in range(size_field):
+        if field[0][i] != value_default:
+            for k in range(1, size_field):
+                if field[k][i] != field[0][i]:
+                    break
+            else:
+                return (f'столбик {i + 1}', field[0][i])
+
+def find_win_diagonal_down(field, value_default):
+    if field[0][0] != value_default:
+        size_field = len(field)
+        for i in range(1, size_field):
+            if field[i][i] != field[0][0]:
+                break
+        else:
+            return ('диагональ ↘', field[0][0])
+
+def find_win_diagonal_up(field, value_default):
+    size_field = len(field)
+    if field[size_field - 1][0] != value_default:
+        for k in range(1, size_field):
+            if field[size_field - 1 - k][k] != field[size_field - 1][0]:
+                break
+        else:
+            return ('диагональ ↗', field[size_field - 1][0])
+
+def check_victory(conditions, field, value_default, char_player_1, player1, player2):
+    item = conditions(field, value_default)
+    if item:
+        if item[1] == char_player_1:
+            print(f'Победа, {item[0]}\nПобедитель {player1}')
+        else:
+            print(f'Победа, {item[0]}\nПобедитель {player2}')
+        return True
 
 # Чтобы могли играть Маша и Саша, например
 dict_players = \
@@ -18,198 +94,80 @@ dict_value = \
         'player2': 'O'
     }
 
-# создание пустого поля заданного размера (квадратного)
-size_row = 3
-size_col = size_row
+# создание пустого поля (квадратного) заданного размера
+size_field = 3
 field = []
-for i in range(size_row):
+for i in range(size_field):
     field.append([])
-    for k in range(size_col):
+    for k in range(size_field):
         field[i].append(dict_value['not'])
 
 # Печать игрового поля с нумерацией
-for i in range(-1, size_row):
-    if i == -1:
-        print(' ', end=' ')
-        for p in range(1, size_col + 1):
-            print(p, end=' ')
-        print()
-    else:
-        print(f'{i + 1}', end=' ')
-        for k in range(0, size_col):
-            print(field[i][k], end=' ')
-        print()
+print('Играем в игру Крестики-Нолики)')
+print(f'Размер игрового поля {size_field}x{size_field}')
+print_field_square(field)
+
+print(f"{dict_players['player1']} - {dict_value['player1']}\n"
+      f"{dict_players['player2']} - {dict_value['player2']}")
 
 # кто ходит первым
-first_step = randint(1, 2)
-if first_step == 1:
-    step = dict_players.get('player1')
-    print(f"Начинает игрок: {dict_players.get('player1')}")
+step_now = randint(1, 2)
+print(f'Начинает игрок: ', end='')
+if step_now == 1:
+    print(dict_players.get('player1'))
 else:
-    step = dict_players.get('player2')
-    print(f"Начинает игрок: {dict_players.get('player2')}")
+    print(dict_players.get('player2'))
 
 # сколько раз делать ход
-steps_max = size_row * size_col  # максимальное количество шагов (клеток)
+steps_max = size_field ** 2  # максимальное количество шагов (ячеек)
 while steps_max > 0:
 
-    # передача хода
-    if step == dict_players.get('player1'):
+    if step_now == 1:
+        # ход игрока
         user_step_ok = False
-
-        # сделать ход игроку
         while not user_step_ok:
             pos_row = int(input('Строка: '))
-            pos_col = int(input('Столбец: '))
-            if not (1 <= pos_row <= size_row and 1 <= pos_col <= size_col):
+            pos_col = int(input('Столбик: '))
+            if not (1 <= pos_row <= size_field and 1 <= pos_col <= size_field):
                 print('Неверные данные, повторите ввод')
             elif field[pos_row - 1][pos_col - 1] == dict_value['not']:
                 field[pos_row - 1][pos_col - 1] = dict_value['player1']
                 user_step_ok = True
             else:
                 print('Ячейка занята, повторите ввод')
+        step_now = 2
 
         print(dict_players.get('player1'))
-        # Печать игрового поля с нумерацией
-        for i in range(-1, size_row):
-            if i == -1:
-                print(' ', end=' ')
-                for p in range(1, size_col + 1):
-                    print(p, end=' ')
-                print()
-            else:
-                print(f'{i + 1}', end=' ')
-                for k in range(0, size_col):
-                    print(field[i][k], end=' ')
-                print()
-        step = dict_players.get('player2')
+        print_field_square(field)
 
     else:
         # Ход компьютера
         comp_step_ok = False
-
         while not comp_step_ok:
-            pos_row = randint(0, size_row - 1)
-            pos_col = randint(0, size_col - 1)
+            pos_row = randint(0, size_field - 1)
+            pos_col = randint(0, size_field - 1)
             if field[pos_row][pos_col] == dict_value['not']:
                 field[pos_row][pos_col] = dict_value['player2']
                 comp_step_ok = True
+        step_now = 1
         time.sleep(1)
 
         print(dict_players.get('player2'))
-        # Печать игрового поля с нумерацией
-        for i in range(-1, size_row):
-            if i == -1:
-                print(' ', end=' ')
-                for p in range(1, size_col + 1):
-                    print(p, end=' ')
-                print()
-            else:
-                print(f'{i + 1}', end=' ')
-                for k in range(0, size_col):
-                    print(field[i][k], end=' ')
-                print()
-        step = dict_players.get('player1')
+        print_field_square(field)
 
-    steps_max -= 1
+    # может быть больше одной победной комбинации
+    a = check_victory(find_win_row, field, dict_value['not'], dict_value['player1'],
+                      dict_players['player1'], dict_players['player2'])
+    b = check_victory(find_win_col, field, dict_value['not'], dict_value['player1'],
+                      dict_players['player1'], dict_players['player2'])
+    c = check_victory(find_win_diagonal_down, field, dict_value['not'], dict_value['player1'],
+                      dict_players['player1'], dict_players['player2'])
+    d = check_victory(find_win_diagonal_up, field, dict_value['not'], dict_value['player1'],
+                      dict_players['player1'], dict_players['player2'])
+    if a or b or c or d:
+        steps_max = -1
+    else:
+        steps_max -= 1
 
-    winner = []
-    # проверка заполнения строки
-    for i in range(size_row):
-        if field[i][0] != dict_value['not']:
-            win_char = field[i][0]
-            win = True
-            for k in range(1, size_col):
-                if field[i][k] != win_char:
-                    win = False
-                    break
-            if win:
-                winner.append((i, win_char))
-
-    if len(winner) > 0:
-        print('Есть победитель')
-        for d in range(len(winner)):
-            print(f'Победила строка {winner[d][0] + 1}\nПобедитель {winner[d][1]}')
-        steps_max = 0
-
-    winner = []
-    # проверка заполнения столбца
-    for i in range(size_col):
-        if field[0][i] != dict_value['not']:
-            win_char = field[0][i]
-            win = True
-            for k in range(1, size_row):
-                if field[k][i] != win_char:
-                    win = False
-                    break
-            if win:
-                winner.append((i, win_char))
-
-    if len(winner) > 0:
-        print('Есть победитель')
-        for d in range(len(winner)):
-            print(f'Победил столбик {winner[d][0] + 1}\nПобедитель {winner[d][1]}')
-        steps_max = 0
-
-    winner = []
-    # проверка заполнения диагонали ↘ !!! для квадратного поля
-    if field[0][0] != dict_value['not']:
-        win_char = field[0][0]
-        win = True
-        for k in range(1, size_row):  # в правый нижний угол
-            if field[k][k] != win_char:
-                win = False
-                break
-        if win:
-            winner.append(('↘', win_char))
-
-    if len(winner) > 0:
-        print('Есть победитель')
-        for d in range(len(winner)):
-            print(f'Победила диагональ {winner[d][0]}\nПобедитель {winner[d][1]}')
-        steps_max = 0
-
-    winner = []
-    # проверка заполнения диагонали ↗ !!! для квадратного поля
-    if field[size_row - 1][0] != dict_value['not']:
-        win_char = field[size_row - 1][0]
-        win = True
-        for k in range(1, size_row):  # в правый нижний угол
-            if field[size_row - 1 - k][k] != win_char:
-                win = False
-                break
-        if win:
-            winner.append(('↗', win_char))
-
-    if len(winner) > 0:
-        print('Есть победитель')
-        for d in range(len(winner)):
-            print(f'Победила диагональ {winner[d][0]}\nПобедитель {winner[d][1]}')
-        steps_max = 0
-
-game_over = True
-for i in range(size_row):
-    if dict_value['not'] in field[i]:
-        game_over = False
-        break
-if game_over:
+if steps_max == 0:
     print('Ничья')
-
-# ! Ошибка, не так сработает, если победитель определился в последнем ходе
-# user
-#   1 2 3
-# 1 O X O
-# 2 X - X
-# 3 O X O
-# comp
-#   1 2 3
-# 1 O X O
-# 2 X O X
-# 3 O X O
-# Есть победитель
-# Победила диагональ ↘
-# Победитель O
-# Есть победитель
-# Победила диагональ ↗
-# Победитель O
-# Ничья
