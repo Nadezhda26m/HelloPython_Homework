f0e491eb4b30d5af3b2b9440ca4751b38e428f79
Nadezhda26m <nadezhda-26m@mail.ru>
2022-12-20 11:58:55 +0300

    HW 017 delete dictionary

--- HW017_GameTicTacToe.py
+++ HW017_GameTicTacToe.py
@@ -17,6 +17,15 @@
 from random import randint
 import time
 
+# создать пустое игровое поле заданного размера
+def create_field(size, char='~'):
+    field = []
+    for i in range(size):
+        field.append([])
+        for k in range(size):
+            field[i].append(char)
+    return field
+
 # Печать квадратного игрового поля с нумерацией
 def print_field_square(field):
     size_field = len(field)
@@ -32,6 +41,7 @@ def print_field_square(field):
                 print(field[i][k], end=' ')
             print()
 
+# Проверяем победу по строкам
 def find_win_row(field, value_default):
     size_field = len(field)
     for i in range(size_field):
@@ -42,6 +52,7 @@ def find_win_row(field, value_default):
             else:
                 return (f'строка {i + 1}', field[i][0])
 
+# Проверяем победу по столбцам
 def find_win_col(field, value_default):
     size_field = len(field)
     for i in range(size_field):
@@ -52,6 +63,7 @@ def find_win_col(field, value_default):
             else:
                 return (f'столбик {i + 1}', field[0][i])
 
+# Проверяем победу по диагонали 1
 def find_win_diagonal_down(field, value_default):
     if field[0][0] != value_default:
         size_field = len(field)
@@ -61,6 +73,7 @@ def find_win_diagonal_down(field, value_default):
         else:
             return ('диагональ ↘', field[0][0])
 
+# Проверяем победу по диагонали 2
 def find_win_diagonal_up(field, value_default):
     size_field = len(field)
     if field[size_field - 1][0] != value_default:
@@ -70,6 +83,7 @@ def find_win_diagonal_up(field, value_default):
         else:
             return ('диагональ ↗', field[size_field - 1][0])
 
+# Проверяем условия победы, выводим победителя
 def check_victory(conditions, field, value_default, char_player_1, player1, player2):
     item = conditions(field, value_default)
     if item:
@@ -79,44 +93,29 @@ def check_victory(conditions, field, value_default, char_player_1, player1, play
             print(f'Победа, {item[0]}\nПобедитель {player2}')
         return True
 
-# Чтобы могли играть Маша и Саша, например
-dict_players = \
-    {
-        'player1': 'user',
-        'player2': 'comp'
-    }
-
-# Быстрая замена символа на пустую ячейку или ходы игроков
-dict_value = \
-    {
-        'not': '-',
-        'player1': 'X',
-        'player2': 'O'
-    }
-
-# создание пустого поля (квадратного) заданного размера
-size_field = 3
-field = []
-for i in range(size_field):
-    field.append([])
-    for k in range(size_field):
-        field[i].append(dict_value['not'])
+player1 = 'user'
+player2 = 'comp'
+value_default = '-'
+value_player1 = 'X'
+value_player2 = 'O'
 
 # Печать игрового поля с нумерацией
+size_field = 3
+field = create_field(size_field, value_default)
 print('Играем в игру Крестики-Нолики)')
-print(f'Размер игрового поля {size_field}x{size_field}')
+print(f'Размер игрового поля {size_field} x {size_field}')
 print_field_square(field)
 
-print(f"{dict_players['player1']} - {dict_value['player1']}\n"
-      f"{dict_players['player2']} - {dict_value['player2']}")
+print(f"{player1} - {value_player1}\n"
+      f"{player2} - {value_player2}")
 
 # кто ходит первым
 step_now = randint(1, 2)
 print(f'Начинает игрок: ', end='')
 if step_now == 1:
-    print(dict_players.get('player1'))
+    print(player1)
 else:
-    print(dict_players.get('player2'))
+    print(player2)
 
 # сколько раз делать ход
 steps_max = size_field ** 2  # максимальное количество шагов (ячеек)
@@ -124,20 +123,19 @@ while steps_max > 0:
 
     if step_now == 1:
         # ход игрока
+        print(f'\n{player1} >>>')
         user_step_ok = False
         while not user_step_ok:
             pos_row = int(input('Строка: '))
             pos_col = int(input('Столбик: '))
             if not (1 <= pos_row <= size_field and 1 <= pos_col <= size_field):
                 print('Неверные данные, повторите ввод')
-            elif field[pos_row - 1][pos_col - 1] == dict_value['not']:
-                field[pos_row - 1][pos_col - 1] = dict_value['player1']
+            elif field[pos_row - 1][pos_col - 1] == value_default:
+                field[pos_row - 1][pos_col - 1] = value_player1
                 user_step_ok = True
             else:
                 print('Ячейка занята, повторите ввод')
         step_now = 2
-
-        print(dict_players.get('player1'))
         print_field_square(field)
 
     else:
@@ -146,24 +144,27 @@ while steps_max > 0:
         while not comp_step_ok:
             pos_row = randint(0, size_field - 1)
             pos_col = randint(0, size_field - 1)
-            if field[pos_row][pos_col] == dict_value['not']:
-                field[pos_row][pos_col] = dict_value['player2']
+            if field[pos_row][pos_col] == value_default:
+                field[pos_row][pos_col] = value_player2
                 comp_step_ok = True
         step_now = 1
         time.sleep(1)
-
-        print(dict_players.get('player2'))
+        print(f'\n{player2} >>>')
+        time.sleep(1)
+        print(f'Строка: {pos_row + 1}\nСтолбик: {pos_col + 1}')
+        time.sleep(1)
         print_field_square(field)
 
+    time.sleep(1)
     # может быть больше одной победной комбинации
-    a = check_victory(find_win_row, field, dict_value['not'], dict_value['player1'],
-                      dict_players['player1'], dict_players['player2'])
-    b = check_victory(find_win_col, field, dict_value['not'], dict_value['player1'],
-                      dict_players['player1'], dict_players['player2'])
-    c = check_victory(find_win_diagonal_down, field, dict_value['not'], dict_value['player1'],
-                      dict_players['player1'], dict_players['player2'])
-    d = check_victory(find_win_diagonal_up, field, dict_value['not'], dict_value['player1'],
-                      dict_players['player1'], dict_players['player2'])
+    a = check_victory(find_win_row, field, value_default, value_player1,
+                      player1, player2)
+    b = check_victory(find_win_col, field, value_default, value_player1,
+                      player1, player2)
+    c = check_victory(find_win_diagonal_down, field, value_default, value_player1,
+                      player1, player2)
+    d = check_victory(find_win_diagonal_up, field, value_default, value_player1,
+                      player1, player2)
     if a or b or c or d:
         steps_max = -1
     else:
